c90fb2b35bff65b0e699a3a299af99678355cc6a
Asaduzzaman Noor <asad.noor.antor@gmail.com>
2024-06-29 00:34:09 +0600

    shell.nix update

--- go-effective-concurrency/code/gopath/src/example2/shell.nix
+++ go-effective-concurrency/code/gopath/src/example2/shell.nix
@@ -4,4 +4,16 @@ pkgs.mkShell {
   buildInputs = with pkgs; [
     go
   ];
+  shellHook = ''
+    getrootdir() {
+      local dir=""
+      local os=`uname`
+      case $os in
+        Darwin) dir="/Users/noor/work/Vivasoft/Blogs" ;;
+        Linux)  dir="/home/noor/work/vivasoft-blogs" ;;
+	* ) ;;
+      esac
+      printf "%s" "$dir"
+    }
+  '';
 }
